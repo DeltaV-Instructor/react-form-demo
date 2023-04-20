d2679fcfc79c6843d3a8fbf6c54f8f0da6e5cd47
tektechnologies <info@tektechnologies.com>
2023-04-20 11:33:21 -0500

    add list items to render

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
+import ListGroup from "react-bootstrap/ListGroup";
+import { ListGroupItem } from "react-bootstrap";
+// import Button from "react-bootstrap/Button";
+// import Form from "react-bootstrap/Form";
 
- class App extends React.Component {
+let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "",
+      sortedData: data,
+    };
+  }
+
+  handleSumbit = (event) => {
+    event.preventDefault();
+    //get form data now
+  };
+
+  handleInput = (event) => {
+    //grab the value
+    //add it to state userName
+  };
+
+  handleSelect = (event) => {
+    //grab value from select in the form then if/else to tell even or odd or all.
+  };
+
   render() {
+    console.log(data);
+    let numbers = data.map((number) => {
+      return <ListGroupItem>{number}</ListGroupItem>;
+    });
     return (
-      <div>App</div>
-    )
+      <>
+        <header>
+          <h1>Forms in React</h1>
+        </header>
+        <section className="listNumbers">
+          <ListGroup>{numbers}</ListGroup>
+        </section>
+      </>
+    );
   }
 }
 
